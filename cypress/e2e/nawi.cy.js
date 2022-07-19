9c19a03913d368f10e8a9b7d32e4ee2e3fe0afdb
Beata Najman <najman.bea@gmail.com>
2022-07-19 13:05:45 +0200

    cr fix

--- cypress/e2e/nawi.cy.js
+++ cypress/e2e/nawi.cy.js
@@ -1,44 +1,48 @@
 describe('nawigacja', () => {
-    beforeEach('user logi', () => {
-      cy.visit('/').url().should('eq', 'https://www.saucedemo.com/')
-      cy.get(`[data-test="username"]`).type("standard_user")
-      cy.get(`[data-test="password"]`).type("secret_sauce")
-      cy.get(`[data-test="login-button"]`).click()
+  beforeEach('user logi', () => {
+    cy.visit('/').url().should('eq', 'https://www.saucedemo.com/')
+    cy.get(`[data-test="username"]`).type("standard_user")
+    cy.get(`[data-test="password"]`).type("secret_sauce")
+    cy.get(`[data-test="login-button"]`).click()
+  })
+  
+  it('left menu',() => {
+      cy.get('.bm-burger-button').click()
+      cy.get('.bm-item-list').should('be.visible')
     })
-    it('Sample name f.e. Should open home page and check url', () => {
-      cy.visit('/').url().should('eq', 'https://www.saucedemo.com/')
+
+    it('product to list',() => {
+      cy.get('.inventory_item_name').contains("Sauce Labs Bolt T-Shirt").click()
+      cy.get('[data-test="back-to-products"]').click() 
+      cy.get('.inventory_container').should('be.visible')
     })
 
-    it('left menu',() => {
-        cy.get('.bm-burger-button').click()
-      })
+    it('cart', () => {
+      cy.get('.shopping_cart_link').click()
+      cy.url().should('contain', 'cart.html')
+    })
 
-      it('product to list',() => {
-        cy.get('.inventory_item_name').contains("Sauce Labs Bolt T-Shirt").click()
-        cy.get('[data-test="back-to-products"]').click() 
-      })
+    it('social media',() => {
+      cy.get('.social_facebook').children().should('have.attr', 'href', 'https://www.facebook.com/saucelabs')
+    })
 
-      it('cart', () => {
-        cy.get('.shopping_cart_link').click()
-      })
+    it('all items',() => {
+      cy.get('.inventory_item_name').first().click()
+      cy.get('.bm-burger-button').click()
+      cy.get('.bm-item').contains('All Items').click()
+      cy.url().should('contain', 'inventory.html')
+    })
 
-      it('social media',() => {
-        cy.get('.social_facebook').children().should('have.attr', 'href', 'https://www.facebook.com/saucelabs')
-      })
+    it('about',() => {
+      cy.get('.bm-burger-button').click()
+      cy.get('.bm-item').contains('About').should('have.attr', 'href', 'https://saucelabs.com/')
+  
+    })
 
-      it('all items',() => {
-        cy.get('.bm-burger-button').click()
-        cy.get('.bm-item').contains('All Items').click()
-      })
-
-      it('about',() => {
-        cy.get('.bm-burger-button').click()
-        cy.get('.bm-item').contains('About').click()
-      })
-
-      it('logout',() => {
-        cy.get('.bm-burger-button').click()
-        cy.get('.bm-item').contains('Logout').click()
-      })
+    it('logout',() => {
+      cy.get('.bm-burger-button').click()
+      cy.get('.bm-item').contains('Logout').click()
+      cy.getCookie('session-username').should('not.exist')
+    })
 
 })
